c2d14b08ac027ef22354be1236652466e2b7125a
Hoprikoma <md.del@hotmail.fr>
2019-02-11 10:13:12 +0100

    Ajout de fichiers
    Ajout de template css et de fichier require ( qui inclus tout les fichiers de chaque pages )

---
+++ html/require.php
@@ -0,0 +1,5 @@
+<?php
+echo`
+<link rel="stylesheet" href="../css/bootstrap.css">
+<script  src="https://code.jquery.com/jquery-3.3.1.min.js"  integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="  crossorigin="anonymous"></script>
+`;
