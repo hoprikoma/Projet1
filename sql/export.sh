5d71175c71a002f7b3d6d714f7297a6ab38c7f37
Charbonnier Robin <charbonnierrobin@gmail.com>
2019-02-13 11:26:04 +0100

    Docker update

--- sql/export.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash 
-set -e 
-APP_ENV=development 
-app bundle exec rake database:create 
-mysqldump > /sql/setup.sql
--- sql/setup.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash 
-set -e 
-service mysql start 
-mysql < /sql/projet_v1.sql 
-service mysql stop 
