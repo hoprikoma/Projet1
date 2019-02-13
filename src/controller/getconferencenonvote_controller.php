0751dbc3d94f228d73e4c1dd91fcca206802a54d
Unknown <thomas_almeida@hotmail.fr>
2019-02-13 11:19:10 +0100

    Ajout conférence voter + conférence non voter

--- src/controller/getconferencenonvote_controller.php
+++ src/controller/getconferencenonvote_controller.php
@@ -1,6 +1,25 @@
 <?php
 require_once '../model/vote.php';
+require_once '../model/comference.php';
 
-     $user = new user();
-     $result = $user->deconnexion();
-     echo $result;
+function key_compare_func($a, $b)
+{
+    if ($a === $b) {
+        return 0;
+    }
+    return ($a > $b)? 1:-1;
+}
+     $vote = new vote();
+     $result_vote = $vote->getConferenceVote();
+     $comference = new comference();
+     $result_conf = $comference->get_conf();
+     $x=0;
+     foreach($result_vote as $vote){
+          foreach($result_conf as $conf){
+               if($conf["nom1"]!=$vote["nom1"]){
+                   $result[$x] = $conf;
+                   $x++;
+               }
+          }
+     }
+     echo json_encode($result);
